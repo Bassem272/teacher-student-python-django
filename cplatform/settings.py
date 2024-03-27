784e48d078317ba0b76e906ea53a1b71a7f8b71b
bassem <bassemgog@gmail.com>
2024-03-27 21:39:34 +0200

    kl

--- cplatform/settings.py
+++ cplatform/settings.py
@@ -138,7 +138,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # added
 MEDIA_ROOT = BASE_DIR / "uploads/"
 MEDIA_URL = "media/"
-<<<<<<< HEAD
 
 import firebase_admin
 from firebase_admin import credentials
@@ -146,5 +145,3 @@ from firebase_admin import credentials
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate("F:\cplatform-project-firebase-adminsdk-u0ez0-fec7f8ddd6.json")
 firebase_admin.initialize_app(cred)
-=======
->>>>>>> parent of 4c03f64 (asdfg)
