62ecba3899d25d0c8cc990ba7691b8643e4e6ba7
bassem <bassem.gehad95@yahoo.com>
2024-06-18 03:28:09 +0300

    go

--- articles_1_01/urls.py
+++ articles_1_01/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create_article/', views.create_article, name='create_article'),
-    path('<str:article_id>/', views.get_article, name='get_article'),   
+    path('get_article/<str:article_id>/', views.get_article, name='get_article'),   
     # path('create/', views.create),   
     path('', views.get_all_articles, name='get_all_articles'),
   
